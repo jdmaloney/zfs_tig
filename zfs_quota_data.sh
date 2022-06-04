b92133c2909646755d256b28be824c2a187dc72f
malone12 <malone12@illinois.edu>
2022-06-04 07:44:47 -0500

    Updated to add quota_type tag and other misc. improvements

--- zfs_quota_data.sh
+++ zfs_quota_data.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-zfs_filesets=()
+zfs_datasets=($(/usr/sbin/zfs list -H | awk '{print $1}' | xargs))
 
-for f in ${zfs_filesets[@]}; do
+for f in ${zfs_datasets[@]}; do
 
 while IFS= read -r line; do
 	IFS=" " read -r username bytes_used bytes_quota obj_used obj_quota <<< "${line}"
-	echo "zfs_quota,fs=${f},username=${username} bytes_used=${bytes_used},bytes_quota=${bytes_quota},obj_used=${obj_used},obj_quota=${obj_quota}"
+	echo "zfs_quota,fs=${f},quota_type=user,username=${username} bytes_used=${bytes_used},bytes_quota=${bytes_quota},obj_used=${obj_used},obj_quota=${obj_quota}"
 done < <(/usr/sbin/zfs userspace -Hp ${f} | awk '{print $3" "$4" "$5" "$6" "$7}')
 
 while IFS= read -r line; do
         IFS=" " read -r groupname bytes_used bytes_quota obj_used obj_quota <<< "${line}"
-        echo "zfs_quota,fs=${f},groupname=${groupname} bytes_used=${bytes_used},bytes_quota=${bytes_quota},obj_used=${obj_used},obj_quota=${obj_quota}"
+        echo "zfs_quota,fs=${f},quota_type=group,groupname=${groupname} bytes_used=${bytes_used},bytes_quota=${bytes_quota},obj_used=${obj_used},obj_quota=${obj_quota}"
 done < <(/usr/sbin/zfs groupspace -Hp ${f} | awk '{print $3" "$4" "$5" "$6" "$7}')
 
 while IFS= read -r line; do
         IFS=" " read -r projectid bytes_used bytes_quota obj_used obj_quota <<< "${line}"
-        echo "zfs_quota,fs=${f},projectid=${projectid} bytes_used=${bytes_used},bytes_quota=${bytes_quota},obj_used=${obj_used},obj_quota=${obj_quota}"
+        echo "zfs_quota,fs=${f},quota_type=project,projectid=${projectid} bytes_used=${bytes_used},bytes_quota=${bytes_quota},obj_used=${obj_used},obj_quota=${obj_quota}"
 done < <(/usr/sbin/zfs projectspace -Hp ${f} | awk '{print $2" "$3" "$4" "$5" "$6}')
 
 done
