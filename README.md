aeea360317a7d84494d3d7197e0fe7a81d89f6b5
malone12 <malone12@illinois.edu>
2022-06-04 10:54:04 -0500

    Added zpool health stats

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # ZFS TIG
 Telegraf Checks and Grafana Dashboards for Monitoring ZFS with TIG
 
+Note: These checks are in supplement to the community maintained [[inputs.zfs]] plugin
+
 ## ZFS Quota Data
 This check ingests user, group, and project quota data for specified ZFS datasets for injection into InfluxDB.  This script can be called at any desired interval; recommended default is 15 minutes.
 
 ## ZFS Dataset Stats
 This check ingests properties about each ZFS dataset on the host so utilization numbers and compression ratios can be tracked over time.  
+
+## zpool Health
+This check ingests data about each pool in terms of its health; it parses through `zpool status` output and records all the values reported for each device and tags by pool, device type, and vdev type.  
---
+++ zpool_health.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+tfile=$(mktemp /tmp/zpool.XXXXXX)
+pools=($(/usr/sbin/zpool list | grep -v NAME | awk '{print $1}'))
+
+for p in ${pools[@]}
+do
+	/usr/sbin/zpool status ${p} -p | sed '1,/NAME/d' | sed -n '/errors/q;p' | sed '/^[[:space:]]*$/d' | awk '{print $1" "$2" "$3" "$4" "$5}' > "${tfile}"
+	vdev_type=pool
+	while read l; do
+		IFS=" " read -r zname state readerr writeerr chksmerr <<< "${l}"
+		case ${zname} in
+			${p})
+				echo "zfshealth,pool=${p},device=${zname},type=pool,vdev_type=none,state=${state} readerr=$readerr,writeerr=${writeerr},checksum=${chksmerr}";;
+			raidz*|mirror*|draid*)
+				echo "zfshealth,pool=${p},device=${zname},type=vdev,vdev_type=${vdev_type},state=${state} readerr=$readerr,writeerr=${writeerr},checksum=${chksmerr}";;
+			special|logs|cache)
+				vdev_type=${zname};;
+				*)
+				echo "zfshealth,pool=${p},device=${zname},type=drive,vdev_type=${vdev_type},state=${state} readerr=$readerr,writeerr=${writeerr},checksum=${chksmerr}"
+		esac
+	done < "${tfile}"
+done
+rm -rf ${tfile}
