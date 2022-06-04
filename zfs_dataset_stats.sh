036e528404bbbe0b9454828ab7a347a65cbfb06a
malone12 <malone12@illinois.edu>
2022-06-03 21:13:14 -0500

    Fixed issue with datset stats no printing right

--- zfs_dataset_stats.sh
+++ zfs_dataset_stats.sh
@@ -8,7 +8,7 @@ stat_line=""
 	while IFS= read -r line; do
 		IFS=" " read -r property value <<< "${line}"
 		stat_line="${stat_line},${property}=${value}"
-	done < <(/usr/sbin/zfs get all ${d} -p | awk '$2 == "compressratio" || $2 == "referenced" || $2 == "written" || $2 == "logicalused" || $2 == "logicalreferenced" || $2 == "used" || $2 == "refcompressratio" || $2 == "available" {print $2" "$3}')
+	done < <(/usr/sbin/zfs get all ${d} -p | awk '$2 == "compressratio" || $2 == "referenced" || $2 == "written" || $2 == "logicalused" || $2 == "logicalreferenced" || $2 == "used" || $2 == "refcompressratio" || $2 == "usedbysnapshots" || $2 == "usedbysnapshots" || $2 == "available" {print $2" "$3}')
 	real_stat_line=$(echo ${stat_line} | cut -c 2-)
 	echo "zfs_stats,dataset=${d} ${real_stat_line}"
 done
